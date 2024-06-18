949ebc65707377f6639c3dcef423560c64402fe7
bcharity2024-a <Allan.Xing2024@ecssen.ca>
2024-06-18 09:51:36 -0600

    Update hasGoodNft.ts

--- apps/api/src/routes/badges/hasGoodNft.ts
+++ apps/api/src/routes/badges/hasGoodNft.ts
@@ -1,13 +1,60 @@
 import type { Handler } from 'express';
 import type { Address } from 'viem';
 
-import { GOOD_MEMBERSHIP_NFT_PUBLICATION_ID } from '@good/data/constants';
+import {
+  GOOD_MEMBERSHIP_NFT_PUBLICATION_ID,
+  IS_MAINNET
+} from '@good/data/constants';
+import LensEndpoint from '@good/data/lens-endpoints';
 import logger from '@good/helpers/logger';
-import lensPg from 'src/db/lensPg';
+import axios from 'axios';
 import catchedError from 'src/helpers/catchedError';
-import { CACHE_AGE_INDEFINITE } from 'src/helpers/constants';
+import { CACHE_AGE_INDEFINITE, GOOD_USER_AGENT } from 'src/helpers/constants';
 import { noBody } from 'src/helpers/responses';
 import { getAddress } from 'viem';
+
+const LENS_GRAPHQL_API_V2 = 'https://api-v2.lens.dev/graphql';
+
+const fetchPublications = async () => {
+  const LENS_V2_HAS_ACTED_QUERY = {
+    query: `
+    query Profile($request: PublicationRequest!) {
+  publication(request: $request) {
+    ... on Post {
+      by {
+        id
+        ownedBy {
+          address
+        }
+      }
+    }
+  }
+}
+  `,
+    variables: {
+      request: {
+        forId: GOOD_MEMBERSHIP_NFT_PUBLICATION_ID
+      }
+    }
+  };
+
+  const { data } = await axios.post(
+    IS_MAINNET ? LensEndpoint.Mainnet : LensEndpoint.Testnet,
+    LENS_V2_HAS_ACTED_QUERY,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'User-agent': GOOD_USER_AGENT
+      }
+    }
+  );
+  console.log(data);
+  if (data.data.publication) {
+    return data.data.publication.by;
+  } else {
+    return null;
+  }
+};
 
 export const get: Handler = async (req, res) => {
   const { address, id } = req.query;
@@ -20,21 +67,28 @@ export const get: Handler = async (req, res) => {
     const formattedAddress = address
       ? getAddress(address as Address)
       : undefined;
-    const data = await lensPg.query(
-      `
-        SELECT EXISTS (
-          SELECT 1
-          FROM profile.record p
-          JOIN publication.open_action_module_acted_record o ON p.profile_id = o.acted_profile_id
-          WHERE
-            (p.profile_id = $1 OR p.owned_by = $2)
-            AND o.publication_id = $3
-        ) AS result;
-      `,
-      [id, formattedAddress, GOOD_MEMBERSHIP_NFT_PUBLICATION_ID]
-    );
 
-    const hasGoodNft = data[0]?.result;
+    console.log(formattedAddress);
+    // const variables = {
+    //   id,
+    //   address: formattedAddress
+    // };
+
+    const response: any = await fetchPublications();
+
+    if (!response) {
+      return res.status(200).json({ hasGoodNft: false, success: true });
+    }
+
+    const profile = response.id;
+    const addr = response.ownedBy.address;
+
+    if (!profile || !addr) {
+      logger.info(`No profile found for ${id || formattedAddress}`);
+      return res.status(200).json({ hasGoodNft: false, success: true });
+    }
+
+    const hasGoodNft = profile == id || formattedAddress == addr;
 
     logger.info(`Good NFT badge fetched for ${id || formattedAddress}`);
 
@@ -45,7 +99,8 @@ export const get: Handler = async (req, res) => {
         hasGoodNft ? CACHE_AGE_INDEFINITE : 'no-cache'
       )
       .json({ hasGoodNft, success: true });
-  } catch (error) {
+  } catch (error: any) {
+    logger.error(`Error fetching Good NFT badge for ${id}: ${error.message}`);
     return catchedError(res, error);
   }
 };
