fe136eceedf4ef5a1bbb7f98857b90a11c7d1f02
thejasonminh <123194102+thejasonminh@users.noreply.github.com>
2024-06-19 10:52:20 -0600

    jason accept/deny good request pt.2

--- apps/web/src/components/Profile/Requests.tsx
+++ apps/web/src/components/Profile/Requests.tsx
@@ -1,8 +1,5 @@
 import { Card } from '@good/ui';
-import {
-  DocumentMinusIcon,
-  DocumentPlusIcon
-} from '@heroicons/react/24/outline';
+import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
 const Requests = () => {
@@ -13,39 +10,37 @@ const Requests = () => {
       <div style={{ display: 'flex', gap: '6rem' }}>
         <span>
           <label htmlFor="sortBy">Sort By: </label>
-          <select className="rounded-xl bg-black" id="sortBy" name="Date">
+          <select className="rounded-xl" id="sortBy" name="Date">
             <option selected>Name</option>
             <option>Amount</option>
           </select>
         </span>
         <span>
           <label htmlFor="filterBy">Filter By: </label>
-          <select className="rounded-xl bg-black" id="filterBy" name="Amount">
+          <select className="rounded-xl" id="filterBy" name="Amount">
             <option selected>Hours</option>
             <option>Amount</option>
           </select>
         </span>
       </div>
 
-      <div className="flex w-full items-center space-x-2 rounded-xl border bg-gray-100 px-4 py-2 dark:border-gray-700 dark:bg-gray-900">
-        <span>GOOD Request</span>
-        <button>
-          <DocumentMinusIcon className="size-5" />
-        </button>
-        <button>
-          <DocumentPlusIcon className="size-5" />
-        </button>
+      <div
+        className="flex w-full items-center space-x-2 rounded-xl border bg-gray-100 px-4 py-2 dark:border-gray-700 dark:bg-gray-900"
+        id="goodRequest"
+      >
+        <span className="px-2">Volunteer Name</span>
+        <span className="px-2">Amount</span>
+        <span className="px-2">Hours</span>
+        <span className="px-2">YYYY-MM-DD</span>
+        <span className="py-2 pl-5">
+          <button className="pl-4" style={{ verticalAlign: 'middle' }}>
+            <MinusIcon className="size-5" />
+          </button>
+          <button className="pl-5" style={{ verticalAlign: 'middle' }}>
+            <PlusIcon className="size-5" />
+          </button>
+        </span>
       </div>
-      {/*
-      <button
-          className="flex w-full items-center space-x-2 rounded-xl border bg-gray-100 px-4 py-2 dark:border-gray-700 dark:bg-gray-900"
-          onClick={() => openModal()}
-          type="button"
-        >
-          <PencilSquareIcon className="size-5" />
-          <span>What's new?!</span>
-        </button>
-        */}
     </Card>
   );
 };
