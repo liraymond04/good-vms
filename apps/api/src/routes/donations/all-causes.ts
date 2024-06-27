57197b317503020b94b2d740a1b460db83c3ffd2
Ezra <ezra.huang2024@ecssen.ca>
2024-06-27 10:40:29 -0600

    all causes fix

--- apps/api/src/routes/donations/all-causes.ts
+++ apps/api/src/routes/donations/all-causes.ts
@@ -4,15 +4,8 @@ import logger from '@good/helpers/logger';
 
 import catchedError from '../../helpers/catchedError';
 import prisma from '../../helpers/prisma';
-import { noBody } from '../../helpers/responses';
 
 export const get: Handler = async (req, res) => {
-  const { id } = req.query;
-
-  if (!id) {
-    return noBody(res);
-  }
-
   try {
     const data = await prisma.cause.findMany();
 
