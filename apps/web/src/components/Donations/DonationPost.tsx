d48278e30a2e58716c362afdc95b20553f7ce84f
Lucas <113465142+LucasHYing@users.noreply.github.com>
2024-07-08 14:55:43 -0700

    fundraiser posts modification

--- apps/web/src/components/Donations/DonationPost.tsx
+++ apps/web/src/components/Donations/DonationPost.tsx
@@ -12,7 +12,7 @@ import getProfile from '@good/helpers/getProfile';
 import getPublicationData from '@good/helpers/getPublicationData';
 import { Image } from '@good/ui';
 import cn from '@good/ui/cn';
-import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
+import { CurrencyDollarIcon, HandThumbUpIcon, ChatBubbleBottomCenterTextIcon, ShareIcon } from '@heroicons/react/24/outline';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useState } from 'react';
@@ -95,7 +95,7 @@ const DonationPost: FC<DonationPostProps> = ({ index, length, post }) => {
       )}
     >
       <div className="p-4">
-        <div className="mb-4 flex items-center">
+        <div className="mb-4 flex items-start">
           <Image
             alt={profile.displayName}
             className="h-10 w-10 rounded-full"
@@ -103,36 +103,44 @@ const DonationPost: FC<DonationPostProps> = ({ index, length, post }) => {
             src={getAvatar(post.by)}
             width={10}
           />
-          <div className="ml-3">
-            <div className="text-sm font-semibold">{profile.displayName}</div>
-            <div className="text-xs text-gray-500 dark:text-gray-100">
-              {formatDate(post.createdAt)}
+          <div className="ml-3 flex-1">
+            <div className="flex items-center justify-between">
+              <div>
+                <div className="text-sm font-semibold">{profile.displayName}</div>
+                <div className="text-xs text-gray-500 dark:text-gray-100">
+                  {formatDate(post.createdAt)}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        {/* Post Contents */}
-        <Markup className="mb-4">{postContent}</Markup>
-        {/* Post Attachments/Media */}
-        {hasAttachments && (
-          <div className="mb-4 h-auto w-full rounded-lg">
-            <Attachments asset={postAsset} attachments={postAttachments} />
-          </div>
-        )}
-        <div className="ld-text-gray-500 flex items-center text-sm text-gray-500 dark:text-gray-100">
-          <div
-            className="mr-4 flex items-center"
-            onClick={() => setShowModal(true)}
-          >
-            <CurrencyDollarIcon className="mr-1 size-4" />
-            {donationsCount} Donations
-          </div>
-          <div className="mr-4 flex items-center">
-            <div className="mr-1" />
-            {post.stats.reactions} likes
-          </div>
-          <div className="flex items-center">
-            <div className="mr-1" />
-            {post.stats.comments} comments
+            {/* Post Contents */}
+            <Markup className="mt-2 mb-4">{postContent}</Markup>
+            {/* Post Attachments/Media */}
+            {hasAttachments && (
+              <div className="mb-4 h-auto w-full rounded-lg">
+                <Attachments asset={postAsset} attachments={postAttachments} />
+              </div>
+            )}
+            <div className="mt-6 flex items-center text-sm text-gray-500 dark:text-gray-100">
+              <div
+                className="mr-4 flex items-center"
+                onClick={() => setShowModal(true)}
+              >
+                <CurrencyDollarIcon className="mr-1 size-4" />
+                {donationsCount} Donations
+              </div>
+              <div className="mr-4 flex items-center">
+                <HandThumbUpIcon className="mr-1 size-4" />
+                {post.stats.reactions} likes
+              </div>
+              <div className="mr-4 flex items-center">
+                <ChatBubbleBottomCenterTextIcon className="mr-1 size-4" />
+                {post.stats.comments} comments
+              </div>
+              <div className="flex items-center">
+                <ShareIcon className="mr-1 size-4" />
+                {post.stats.shares} shares
+              </div>
+            </div>
           </div>
         </div>
       </div>
