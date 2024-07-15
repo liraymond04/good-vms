7a5a051b8eda23614dfa2ee80e5e471b21252a4a
LucasHYing <LucasHYing@users.noreply.github.com>
2024-07-15 08:50:48 +0000

    Apply formatting changes

--- apps/web/src/components/Shared/Navbar/MoreNavItems.tsx
+++ apps/web/src/components/Shared/Navbar/MoreNavItems.tsx
@@ -1,13 +1,15 @@
 import type { FC } from 'react';
+
 import cn from '@good/ui/cn';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import {
   BookmarkIcon,
   CurrencyDollarIcon,
   EllipsisHorizontalCircleIcon,
-  UserIcon,
+  UserIcon
 } from '@heroicons/react/24/outline';
 import { useProfileStore } from 'src/store/persisted/useProfileStore';
+
 import MenuTransition from '../MenuTransition';
 import MoreLink from './NavItems/MoreLink';
 import Support from './NavItems/Support';
@@ -25,23 +27,26 @@ const MoreNavItems: FC = () => {
               {
                 'bg-gray-200 text-black dark:bg-gray-800 dark:text-white': open,
                 'text-white-700 dark:text-white-300 hover:bg-gray-200 hover:text-black dark:hover:bg-gray-800 dark:hover:text-white':
-                  !open,
+                  !open
               }
             )}
           >
             <EllipsisHorizontalCircleIcon className="size-8" />
-            <span className="nav-text text-xl text-black dark:text-white hidden md:block">
+            <span className="nav-text hidden text-xl text-black md:block dark:text-white">
               More
             </span>
           </MenuButton>
           <MenuTransition>
             <MenuItems
               className={cn(
-                'absolute z-50 mt-2 w-40 origin-top-left rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-900 dark:ring-gray-700',
-                'md:fixed md:top-48 md:left-40'
+                'absolute z-50 mt-2 w-40 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-900 dark:ring-gray-700',
+                'md:fixed md:left-40 md:top-48'
               )}
               style={{
-                left: window.innerWidth >= 768 && window.innerWidth <= 1024 ? '20px' : 'initial',
+                left:
+                  window.innerWidth >= 768 && window.innerWidth <= 1024
+                    ? '20px'
+                    : 'initial'
               }}
             >
               {currentProfile ? (
@@ -80,7 +85,7 @@ const MoreNavItems: FC = () => {
                               {
                                 'text-gray-700 dark:text-gray-200': open,
                                 'text-gray-700 hover:bg-gray-200 hover:text-black dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white':
-                                  !open,
+                                  !open
                               }
                             )}
                           >
@@ -90,7 +95,9 @@ const MoreNavItems: FC = () => {
                             </span>
                           </MenuButton>
                           <MenuTransition>
-                            <MenuItems className="absolute z-50 left-full top-0 ml-2 w-32 origin-top-left rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-900 dark:ring-gray-700 p-1"> {/* Adjusted width to 32 and padding */}
+                            <MenuItems className="absolute left-full top-0 z-50 ml-2 w-32 origin-top-left rounded-md bg-white p-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-900 dark:ring-gray-700">
+                              {' '}
+                              {/* Adjusted width to 32 and padding */}
                               <MenuItem
                                 as="div"
                                 className={({ focus }: { focus: boolean }) =>
