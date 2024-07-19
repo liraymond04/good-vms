075b2df8621a487d87658cd065953fdee230ec28
Nathan <mangoldn0@gmail.com>
2024-07-19 15:36:26 -0600

    Fixed client crashing when user is not signed in

--- packages/helpers/getAvatar.ts
+++ packages/helpers/getAvatar.ts
@@ -4,14 +4,12 @@ import getLennyURL from './getLennyURL';
 import imageKit from './imageKit';
 import sanitizeDStorageUrl from './sanitizeDStorageUrl';
 
-/**
- * Returns the avatar image URL for a given profile.
- *
- * @param profile The profile object.
- * @param namedTransform The named transform to use.
- * @returns The avatar image URL.
- */
 const getAvatar = (profile: any, namedTransform = AVATAR): string => {
+  if (!profile?.id) {
+    // Handle case where profile ID is null or undefined
+    return getLennyURL('default'); // Return a default image URL or placeholder
+  }
+
   const avatarUrl =
     // Lens NFT Avatar fallbacks
     profile?.metadata?.picture?.image?.optimized?.uri ||
